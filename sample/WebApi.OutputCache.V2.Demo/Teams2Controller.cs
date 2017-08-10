76a4ddf56e7b3ba09a7a68cbbb5772c990f8909c
Jonathan Rubin Yaniv <jorub@microsoft.com>
2017-08-10 21:16:20 +0300

    minor

--- sample/WebApi.OutputCache.V2.Demo/Teams2Controller.cs
+++ sample/WebApi.OutputCache.V2.Demo/Teams2Controller.cs
@@ -3,26 +3,22 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using WebApi.OutputCache.V2.TimeAttributes;
 
 namespace WebApi.OutputCache.V2.Demo
 {
-    [AutoInvalidateCacheOutput]
     public class Teams2Controller : ApiController
     {
         private static readonly List<Team> Teams = new List<Team>
-            {
-                new Team {Id = 1, League = "NHL", Name = "Leafs"},
-                new Team {Id = 2, League = "NHL", Name = "Habs"},
-            };
+        {
+            new Team {Id = 1, League = "NHL", Name = "Leafs"},
+            new Team {Id = 2, League = "NHL", Name = "Habs"},
+        };
 
-        [CacheOutput(ClientTimeSpan = 50, ServerTimeSpan = 50)]
         public IEnumerable<Team> Get()
         {
             return Teams;
         }
 
-        [CacheOutputUntil(2014, 7, 20)]
         public Team GetById(int id)
         {
             var team = Teams.FirstOrDefault(i => i.Id == id);
@@ -33,13 +29,15 @@ namespace WebApi.OutputCache.V2.Demo
 
         public void Post(Team value)
         {
-            if (!ModelState.IsValid) throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState));
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState));
             Teams.Add(value);
         }
 
         public void Put(int id, Team value)
         {
-            if (!ModelState.IsValid) throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState));
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState));
 
             var team = Teams.FirstOrDefault(i => i.Id == id);
             if (team == null) throw new HttpResponseException(HttpStatusCode.NotFound);
