9fe234c90bcae705b89d426262ffbfcb5fa96907
Jonathan Rubin Yaniv <jorub@microsoft.com>
2017-08-12 00:36:01 +0300

    Added missing file

---
+++ sample/WebApi.OutputCache.V2.Demo/Core/IgnoreCache.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Web.Http.Filters;
+
+namespace WebApi.OutputCache.V2.Demo.Core
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    public class IgnoreCache : ActionFilterAttribute
+    {
+        // No logic
+    }
+}
