933cd8eb298a1c86baafb99743aff836d7d91444
Jonathan Rubin Yaniv <jorub@microsoft.com>
2017-08-11 20:37:44 +0300

    Fixed a bug in Unity initialization

--- sample/WebApi.OutputCache.V2.Demo/Program.cs
+++ sample/WebApi.OutputCache.V2.Demo/Program.cs
@@ -3,6 +3,7 @@ using System.Web.Http;
 using System.Web.Http.SelfHost;
 using Microsoft.Practices.Unity;
 using Microsoft.Practices.Unity.WebApi;
+using WebApi.OutputCache.V2.Demo.App_Start;
 
 namespace WebApi.OutputCache.V2.Demo
 {
@@ -10,28 +11,25 @@ namespace WebApi.OutputCache.V2.Demo
     {
         public static void Main(string[] args)
         {
-            using (var container = new UnityContainer())
+            HttpSelfHostConfiguration config = new HttpSelfHostConfiguration("http://localhost:8000")
             {
-                HttpSelfHostConfiguration config = new HttpSelfHostConfiguration("http://localhost:8000")
-                {
-                    DependencyResolver = new UnityDependencyResolver(container)
-                };
+                DependencyResolver = new UnityDependencyResolver(UnityConfig.GetConfiguredContainer())
+            };
 
-                config.MapHttpAttributeRoutes();
-                config.Routes.MapHttpRoute(
-                    name: "DefaultApi",
-                    routeTemplate: "api/{controller}/{id}",
-                    defaults: new {id = RouteParameter.Optional}
-                );
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new {id = RouteParameter.Optional}
+            );
 
-                HttpSelfHostServer server = new HttpSelfHostServer(config);
-                //            config.CacheOutputConfiguration().RegisterCacheOutputProvider(() => new MemoryCacheDefault());
+            HttpSelfHostServer server = new HttpSelfHostServer(config);
+            //            config.CacheOutputConfiguration().RegisterCacheOutputProvider(() => new MemoryCacheDefault());
 
-                server.OpenAsync().Wait();
+            server.OpenAsync().Wait();
 
-                Console.ReadKey();
-                server.CloseAsync().Wait();
-            }
+            Console.ReadKey();
+            server.CloseAsync().Wait();
         }
     }
 }
