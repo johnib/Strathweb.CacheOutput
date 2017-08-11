d434a5faa4c0076e644804cdca199d7b31639dfd
Jonathan Rubin Yaniv <jorub@microsoft.com>
2017-08-12 01:48:15 +0300

    Added Redis implementation for the IOutputCache interface

--- WebApi.OutputCache.Demo.Tests/InMemoryOutputCacheTests.cs
+++ WebApi.OutputCache.Demo.Tests/InMemoryOutputCacheTests.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Linq;
 using System.Runtime.Caching;
+using System.Text;
 using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StackExchange.Redis;
 using WebApi.OutputCache.V2.Demo.Core;
 
 namespace WebApi.OutputCache.Demo.Tests
@@ -11,33 +14,51 @@ namespace WebApi.OutputCache.Demo.Tests
     {
         private const string DefaultKey = "key";
         private const string DefaultValue = "value";
+        private readonly byte[] _defaultValueBytes = Encoding.UTF8.GetBytes(DefaultValue);
 
-        private IOutputCache<string> _cacheUnderTest;
+        private IOutputCache<byte[]> _cacheUnderTest;
+        private static ConnectionMultiplexer _connection;
+        private static IDatabase _database;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            const string connectionString =
+                "localhost:6379,password=,ssl=False,abortConnect=False";
+
+            ConfigurationOptions redisConfig = ConfigurationOptions.Parse(connectionString);
+            redisConfig.AllowAdmin = true;
+            _connection = ConnectionMultiplexer.Connect(redisConfig);
+            _database = _connection.GetDatabase();
+        }
 
         [TestInitialize]
         public void Initialize()
         {
-            _cacheUnderTest = new InMemoryOutputCache<string>(new MemoryCache("test"));
+//            _cacheUnderTest = new InMemoryOutputCache<byte[]>(new MemoryCache("test"));
+
+            _connection.GetServer(_connection.GetEndPoints().First()).FlushDatabase();
+            _cacheUnderTest = new RedisOutputCache(_connection, _database);
         }
 
         [TestMethod]
         public void TestAdd()
         {
-            _cacheUnderTest.Set(DefaultKey, DefaultValue, DateTimeOffset.MaxValue);
-            var value = _cacheUnderTest.Get(DefaultKey);
+            _cacheUnderTest.Set(DefaultKey, _defaultValueBytes, DateTimeOffset.MaxValue);
 
-            Assert.AreEqual(DefaultValue, value);
+            Assert.AreEqual(DefaultValue, Encoding.UTF8.GetString(_cacheUnderTest.Get(DefaultKey)));
         }
 
         [TestMethod]
         public void TestSetOverridesExistingKey()
         {
             const string existingValue = "existingValue";
-            _cacheUnderTest.Set(DefaultKey, existingValue, DateTimeOffset.MaxValue);
-            _cacheUnderTest.Set(DefaultKey, DefaultValue, DateTimeOffset.MaxValue);
+            byte[] existingValueBytes = Encoding.UTF8.GetBytes(existingValue);
+            _cacheUnderTest.Set(DefaultKey, existingValueBytes, DateTimeOffset.MaxValue);
+            _cacheUnderTest.Set(DefaultKey, _defaultValueBytes, DateTimeOffset.MaxValue);
             var currentValue = _cacheUnderTest.Get(DefaultKey);
 
-            Assert.AreEqual(DefaultValue, currentValue);
+            Assert.AreEqual(DefaultValue, Encoding.UTF8.GetString(currentValue));
         }
 
         [TestMethod]
@@ -51,7 +72,7 @@ namespace WebApi.OutputCache.Demo.Tests
         [TestMethod]
         public void TestContainsWhenKeyDoesExist()
         {
-            _cacheUnderTest.Set(DefaultKey, DefaultValue, DateTimeOffset.MaxValue);
+            _cacheUnderTest.Set(DefaultKey, _defaultValueBytes, DateTimeOffset.MaxValue);
             var itemExists = _cacheUnderTest.Contains(DefaultKey);
 
             Assert.IsTrue(itemExists);
@@ -60,16 +81,16 @@ namespace WebApi.OutputCache.Demo.Tests
         [TestMethod]
         public void TestGetDoesNotRemoveCacheEntry()
         {
-            _cacheUnderTest.Set(DefaultKey, DefaultValue, DateTimeOffset.MaxValue);
+            _cacheUnderTest.Set(DefaultKey, _defaultValueBytes, DateTimeOffset.MaxValue);
 
-            Assert.AreEqual(DefaultValue, _cacheUnderTest.Get(DefaultKey));
-            Assert.AreEqual(DefaultValue, _cacheUnderTest.Get(DefaultKey));
+            Assert.AreEqual(DefaultValue, Encoding.UTF8.GetString(_cacheUnderTest.Get(DefaultKey)));
+            Assert.AreEqual(DefaultValue, Encoding.UTF8.GetString(_cacheUnderTest.Get(DefaultKey)));
         }
 
         [TestMethod]
         public void TestRemove()
         {
-            _cacheUnderTest.Set(DefaultKey, DefaultValue, DateTimeOffset.MaxValue);
+            _cacheUnderTest.Set(DefaultKey, _defaultValueBytes, DateTimeOffset.MaxValue);
             _cacheUnderTest.Remove(DefaultKey);
             var itemExists = _cacheUnderTest.Contains(DefaultKey);
 
@@ -82,9 +103,9 @@ namespace WebApi.OutputCache.Demo.Tests
             const string key1 = "key1";
             const string key2 = "key2";
 
-            _cacheUnderTest.Set(DefaultKey, DefaultValue, DateTimeOffset.MaxValue);
-            _cacheUnderTest.Set(key1, DefaultValue, DateTimeOffset.MaxValue, DefaultKey);
-            _cacheUnderTest.Set(key2, DefaultValue, DateTimeOffset.MaxValue, DefaultKey);
+            _cacheUnderTest.Set(DefaultKey, _defaultValueBytes, DateTimeOffset.MaxValue);
+            _cacheUnderTest.Set(key1, _defaultValueBytes, DateTimeOffset.MaxValue, DefaultKey);
+            _cacheUnderTest.Set(key2, _defaultValueBytes, DateTimeOffset.MaxValue, DefaultKey);
             _cacheUnderTest.RemoveDependentsOf(DefaultKey);
 
             var key1Exists = _cacheUnderTest.Contains(key1);
@@ -97,7 +118,7 @@ namespace WebApi.OutputCache.Demo.Tests
         [TestMethod]
         public void TestCacheKeyExpiration()
         {
-            _cacheUnderTest.Set(DefaultKey, DefaultValue, DateTimeOffset.UtcNow + TimeSpan.FromMilliseconds(50));
+            _cacheUnderTest.Set(DefaultKey, _defaultValueBytes, DateTimeOffset.UtcNow + TimeSpan.FromMilliseconds(50));
             Thread.Sleep(100);
 
             var doesExist = _cacheUnderTest.Contains(DefaultKey);
---
+++ sample/WebApi.OutputCache.V2.Demo/Core/RedisOutputCache.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace WebApi.OutputCache.V2.Demo.Core
+{
+    public class RedisOutputCache : IOutputCache<byte[]>
+    {
+        private const string DependencySetPrefix = "set:";
+        private readonly IConnectionMultiplexer _multiplexer;
+        private readonly IDatabase _redis;
+
+        public RedisOutputCache(IConnectionMultiplexer connection, int database = -1)
+            : this(connection, connection.GetDatabase())
+        {
+        }
+
+        public RedisOutputCache(IConnectionMultiplexer connection, IDatabase database)
+        {
+            _multiplexer = connection;
+            _redis = database;
+        }
+
+        public void Set(string key, byte[] content, DateTimeOffset expiration, string dependsOnKey = null)
+        {
+            bool wasAdded = _redis.StringSet(key, content, expiration.Subtract(DateTimeOffset.UtcNow));
+
+            if (wasAdded && !string.IsNullOrEmpty(dependsOnKey))
+            {
+                // TODO: retry on failures
+                var setKey = $"{DependencySetPrefix}{dependsOnKey}";
+                _redis.SetAdd(setKey, key, CommandFlags.HighPriority);
+                _redis.KeyExpire(setKey, expiration.Subtract(DateTimeOffset.UtcNow), CommandFlags.HighPriority);
+            }
+        }
+
+        public bool Contains(string key)
+        {
+            return _redis.KeyExists(key);
+        }
+
+        public byte[] Get(string key)
+        {
+            return _redis.StringGet(key);
+        }
+
+        public void Remove(string key)
+        {
+            _redis.KeyDelete(key);
+        }
+
+        public void RemoveDependentsOf(string key)
+        {
+            RedisValue[] dependents = _redis.SetMembers($"{DependencySetPrefix}{key}");
+
+            foreach (var dependentKey in dependents)
+            {
+                Remove(dependentKey);
+            }
+        }
+    }
+}
